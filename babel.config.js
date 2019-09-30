b92f82eb7ed0b873572e60b947c487080755f52b
ihupoo <840499492@qq.com>
2019-09-30 16:12:28 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugins": [
         "@babel/plugin-syntax-dynamic-import",
         "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-object-rest-spread",
         "@babel/plugin-proposal-decorators",
         "@babel/plugin-proposal-class-properties",
         ['@babel/plugin-transform-runtime',
