20ae3450b49169d8e6e13bd543ff4f34972bc251
ihupoo <840499492@qq.com>
2019-09-30 15:57:06 +0800

    add

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "presets": [
+        [
+            "@babel/preset-env"
+        ],
+    ],
+    "plugins": [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-decorators",
+        "@babel/plugin-proposal-class-properties",
+        ['@babel/plugin-transform-runtime',
+            {
+                'corejs': 3,
+            },
+        ],
+    ]
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    // 处理 @import
+    'postcss-import': {},
+    // 处理 css 中 url
+    'postcss-url': {},
+    // 自动前缀
+    'autoprefixer': {
+        "browsers": [
+            "> 1%",
+            "last 2 versions",
+            "Firefox ESR",
+            "not ie <= 8"
+        ]
+    }
   }
 }
